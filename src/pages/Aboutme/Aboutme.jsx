f2abc34de5b53ca7da30fef06096bb6a961a9a4a
Mansoor-P <munaf6007@gmail.com>
2024-03-23 14:39:54 +0530

    aboutme updated

--- src/pages/Aboutme/Aboutme.jsx
+++ src/pages/Aboutme/Aboutme.jsx
@@ -1,140 +1,146 @@
 import React from "react";
 import "./Aboutme.css";
-import { Link } from "react-router-dom";
+import { projects, education } from "./aboutmeData";
+
 const Aboutme = () => {
-  const skills = [
-    "Java",
-    "SQL",
-    "MySQL",
-    "DSA",
-    "ReactJS",
-    "HTML5",
-    "CSS3",
-    "JavaScript",
-    "Bootstrap",
-    "J2EE",
-    "JSP",
-    "JDBC",
-    "Database Management System",
-    "Operating System",
-    "Networking Concepts",
-    "System Design",
-    "Eclipse IDE",
-    "Git",
-    "GitHub",
-  ];
-
   return (
-    <>
-      <Link className="home-link" to="/">HOME</Link>
-      <div className="aboutme-container">
-        <div className="left-section">
-          <section className="profile-section">
-            <div className="container">
-              <div className="picture-resume-wrapper">
-                <div className="picture-resume">
-                  <img src="../assets/aboutr.jpg" alt="" />
-                </div>
-                <div className="name-wrapper">
-                  <h1> Mansoor Pathikonda </h1>
-                </div>
-              </div>
-
-              <div className="contact-info  ">
-                <ul className="list-titles">
-                  <li>Call</li>
-                </ul>
-                <ul className="list-titles">
-                  {" "}
-                  <li>Mail</li>
-                </ul>
-                <ul className="list-titles">
-                  <li>LinkedIn</li>
-                </ul>
-                <ul className="list-titles">
-                  <li>GitHub</li>
-                </ul>
-              </div>
-              <div className="contact-presentation">
-                <p>
-                  As a passionate and driven fresher, I am deeply committed to
-                  leveraging technology for positive change in the world. I
-                  believe that with innovation, dedication, and a strong sense
-                  of purpose, we can address critical issues, improve lives, and
-                  create a brighter future for everyone. I am eager to learn,
-                  grow, and contribute my skills and ideas to meaningful
-                  projects that make a difference and inspire others to join in
-                  the journey of transforming our world through technology.
-                  Passionate about changing the world with technology.
-                </p>
-              </div>
-              <div class="section-wrapper">
-                <h4>Hobbies</h4>
-                <ul>
-                  <li>Coding daily</li>
-                  <li>Problem solving</li>
-                  <li>Web crawling</li>
-                  <li>Playing cricket</li>
-                </ul>
-              </div>
+    <div className="container-fluid">
+      <div className="row mt-4 mb-4">
+        <div className="col-md-4 left ">
+          <div className="left-side">
+            <div className="profile">
+              <img src="public/assets/aboutr.jpg" alt="Mansoor" />
+              <h4>Mansoor Pathikonda</h4>
             </div>
-          </section>
-        </div>
-
-        <div className="right-section">
-          <section className="education-section">
-            <h3 className="education-title">Education</h3>
-            <div className="education-wrapper">
-              <div className="educational-details">
-                <div className="education-title">
-                  <h2>
-                    MCA <span>2021-2023</span>
-                  </h2>
-                  <p>
-                    <i>Sri Venkateswara University , Tirupati</i>
-                  </p>
-                  <p className="education-description">
-                    This is my latest graduation where I pursued my Master of
-                    Computer Applications (MCA) from Sri Venkateswara
-                    University, Tirupati. It was a transformative experience
-                    that enhanced my skills and knowledge in the field of
-                    computer science and technology.
-                  </p>
-                </div>
-                <div className="education-title">
-                  <h2>
-                    BS.c <span>2018-2021</span>
-                  </h2>
-                  <p>
-                    <i>The Adoni Arts and Science College , Adoni</i>
-                  </p>
-                </div>
-                <div className="education-title">
-                  <h2>
-                    XII <span>2016-2018</span>
-                  </h2>
-                  <p>
-                    <i>Sri Satya Sai Junior College , Ananthapur</i>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="skills">
+            <div className="profile-summary">
               <p>
-                {skills.map((skill, index) => (
-                  <React.Fragment key={skill}>
-                    <span className="skill">{skill}</span>
-                    {index !== skills.length - 1 && (
-                      <span className="separator">|</span>
-                    )}
-                  </React.Fragment>
-                ))}
+                As a passionate and driven fresher, I am deeply committed to
+                leveraging technology for positive change in the world. I
+                believe that with innovation, dedication, and a strong sense of
+                purpose, we can address critical issues, improve lives, and
+                create a brighter future for everyone. I am eager to learn,
+                grow, and contribute my skills and ideas to meaningful projects
+                that make a difference and inspire others to join in the journey
+                of transforming our world through technology. Passionate about
+                changing the world with technology.
               </p>
             </div>
-          </section>
+          </div>
+        </div>
+        <div className="col-md-8 right border border-dark rounded   ">
+          <div className="resume-header">
+            <h1>Mansoor Pathikonda</h1>
+            <div className="resume-header-links">
+              <ul>
+                <li>+91 9704039210</li>
+                <li><a href="">mansoorpathikonda@gmail.com</a></li>
+                <li><a href="">LinkedIn</a></li>
+                <li><a href="">GitHub</a></li>
+                <li><a href="">Portfolio</a></li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+          <div className="professional-summary">
+            <h3>Professional Summary</h3>
+            <p>
+              Passionate fresh software developer skilled in building robust
+              applications from scratch. Completed a project showcasing
+              problem-solving abilities, experienced in various technologies,
+              ready to contribute effectively to development teams.
+            </p>
+          </div>
+
+          <hr />
+          <div className="projects">
+            <h3>Projects</h3>
+            {projects.map((project, index) => (
+              <div key={index} className="project-item">
+                <div className="row mx-2 ">
+                  <div className="col-8 project-title">
+                    <h4>
+                      {project.name}{" "}
+                      <span>
+                        <a href={project.liveDemoLink}>Live Demo</a>
+                      </span>
+                    </h4>
+                  </div>
+                  <div className="col-4">
+                    <p> {project.timeframe}</p>
+                  </div>
+                  <ul className="mx-4">
+                    <li className="mx-2">
+                      {" "}
+                      <p>{project.description}</p>
+                    </li>
+                    <li className="mx-2">
+                      {" "}
+                      <p>
+                        <strong>Technologies:</strong>{" "}
+                        {project.technologies.join(", ")}
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <hr />
+          <div className="education">
+            <h3>Education</h3>
+            {education.map((edu, index) => (
+              <div key={index} className="education-item">
+                <div className="row mx-2 ">
+                  <div className="col-10 education-title">
+                    <h5>
+                      {edu.degree}
+                      <span>
+                        {" - "}
+                        {edu.percentage}
+                      </span>
+                    </h5>
+                  </div>
+                  <div className="col-2">
+                    <p> {edu.timeframe}</p>
+                  </div>
+                  <p>
+                    {edu.institution}, {edu.location}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <hr />
+          <div className="skills">
+            <h3>Skills</h3>
+            <div className="skills">
+              <p>
+                <strong>Languages: </strong> <mark>Java</mark> , SQL, HTML/CSS, JavaScript,
+                React.js, Bootstrap, MongoDB
+              </p>
+            </div>
+            <div className="skills">
+              <p>
+                {" "}
+                <strong>Course Work: </strong>
+                <mark> DSA – Data structures and Algorithms </mark>, <mark>Linux</mark> , Database Management
+                System, Networking, Operating System, <mark>System Design</mark>, Relational
+                Database, Full stack Development
+              </p>
+            </div>
+            <div className="skills">
+              <p>
+                {" "}
+                <strong>Developer Tools: </strong>
+                IntelliJ, Vs-Code, <mark>Figma</mark> , Git, GitHub, My-SQL Workbench
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/pages/Aboutme/aboutmeData.js
@@ -0,0 +1,54 @@
+
+const projects = [
+  {
+    name: "Food Delivery App",
+    technologies: ["React", "HTML/CSS", "JavaScript"],
+    timeframe: "Feb 2024 - Present",
+    description:
+      "Developed a food delivery app with authentication, order tracking, payment integration, and API data integration. Ensured optimal user experience with responsive design.",
+    liveDemoLink: "Live Demo Link Here",
+  },
+  {
+    name: "Job Searching Portal",
+    technologies: ["React", "HTML/CSS", "JavaScript"],
+    timeframe: "Nov 2023 - Jan 2024",
+    description:
+      "Successfully completed full-stack solution for job searching portal, crafting an intuitive UI with HTML, CSS, and JavaScript for dynamic form interactions, real-time previews, and resume section management.",
+    liveDemoLink: "Live Demo Link Here",
+  },
+  {
+    name: "Ecommerce Web App",
+    technologies: ["Java", "JSP", "JDBC", "My-SQL"],
+    timeframe: "Jul 2023 - Sep 2023",
+    description:
+      "Hands-on experience in developing end-to-end e-commerce solutions, including user authentication, product catalog management, shopping cart functionality, and secure payment gateways integration. Proficient in writing clean and maintainable code following best practices and design patterns.",
+    liveDemoLink: "Live Demo Link Here",
+  },
+];
+
+const education = [
+  {
+    degree: "Master of Computer Applications (MCA)",
+    institution: "University Campus – S.V. University",
+    location: "Tirupati, A.P",
+    percentage: "81%",
+    timeframe: "2021 – 2023",
+  },
+  {
+    degree: "Bachelor of Science (B.Sc)",
+    institution: "The Adoni Arts & Science College",
+    location: "Adoni, Kurnool Dist. A.P",
+    percentage: "77%",
+    timeframe: "2018 - 2021",
+  },
+  {
+    degree: "Class XII - Mathematics, Physics, Chemistry (MPC)",
+    institution: "Sri Satya Sai Junior College",
+    location: "Anantapur, A.P",
+    percentage: "93.1%",
+    timeframe: "2016 - 2018",
+  },
+];
+
+
+export { projects, education };
