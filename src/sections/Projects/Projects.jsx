afb2da6ae8d7e882321e4a065476c13191333246
Mansoor-P <munaf6007@gmail.com>
2024-06-27 12:40:29 +0530

    projects added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,9 @@ import styles from "./ProjectsStyles.module.css";
 import job from "../../assets/job.svg";
 import food from "../../assets/food.svg";
 import portfolio from "../../assets/portfolio.svg";
+import Ecart from "../../assets/E-cart.svg";
+import Hotel from "../../assets/hotel.svg";
+
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -27,6 +30,19 @@ function Projects() {
           h3="Portfolio"
           p="portfolio"
         />
+        <ProjectCard
+          src={Ecart}
+          link="https://github.com/Mansoor-P/E-Commerce-Website"
+          h3="E-Cart"
+          p="E-cart"
+        />
+        <ProjectCard
+          src={Hotel}
+          link="https://github.com/Mansoor-P/hotel_booking"
+          h3="Hotel Booking Aeb App"
+          p="Hotel Booking Aeb App"
+        />
+     
       </div>
     </section>
   );
